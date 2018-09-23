8c28a8285b13200adb75efda139b5983655d8675
Raxkas <rxrhhx@gmail.com>
2018-09-23 23:07:04 +0400

    Make MobBasket.mobs protected

--- mob_basket.py
+++ mob_basket.py
@@ -1,36 +1,34 @@
-# TODO: immutable version?
 class MobBasket:
-    """Purchases for the wave. """
+    """Purchases for the wave."""
 
-    __slots__ = ("mobs", "income_increase", "money_cost", "power_cost")
+    __slots__ = ("_mobs", "income_increase", "money_cost", "power_cost")
 
     def __init__(self):
-        self.mobs = ()
+        self._mobs = ()
         self.income_increase = 0
         self.money_cost = 0
         self.power_cost = 0
 
     def __iter__(self):
-        return iter(self.mobs)
+        return iter(self._mobs)
 
     # TODO: use copy module?
     def copy(self):
         copy = self.__class__()
-        copy.mobs = self.mobs
+        copy._mobs = self._mobs
         copy.income_increase = self.income_increase
         copy.money_cost = self.money_cost
         copy.power_cost = self.power_cost
         return copy
 
     def merge(self, other):
-        self.mobs += other.mobs
+        self._mobs += other.mobs
         self.income_increase += other.income_increase
         self.money_cost += other.money_cost
         self.power_cost += other.power_cost
 
     def buy(self, mob):
-        # self.mobs[mob] += 1
-        self.mobs += (mob,)
+        self._mobs += (mob,)
         self.income_increase += mob.income
         self.money_cost += mob.money_cost
         self.power_cost += mob.power_cost
